464814047c233ed3648fd0f749f66709d977241b
Fernando Pasik <fernando@pasik.com.ar>
2018-02-16 22:40:58 +0000

    fix: Cache in service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('fernandopasik').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        '/style.css',
+        '/fernando-pasik.png',
+        '/fernando-pasik.webp'
+      ]);
+    })
+  );
+});
