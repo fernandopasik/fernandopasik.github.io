61ebea4de87d71088abcde87ca0a21143cc71ed8
Fernando Pasik <fernando@pasik.com.ar>
2026-03-28 11:59:21 +0000

    style: update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import prettier from 'eslint-config-prettier';
 import ymlPlugin from 'eslint-plugin-yml';
+import { defineConfig } from 'eslint/config';
 
-export default [
+export default defineConfig([
   ...ymlPlugin.configs['flat/recommended'],
   ...ymlPlugin.configs['flat/prettier'],
   prettier,
-];
+]);
