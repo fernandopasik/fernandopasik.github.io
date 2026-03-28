ec1edd69a6e48938e2a2a46007b6b5706fd7c5c6
Fernando Pasik <fernando@pasik.com.ar>
2026-03-28 12:00:34 +0000

    style: add plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
 import prettier from 'eslint-config-prettier';
+import markdown from '@eslint/markdown';
 import ymlPlugin from 'eslint-plugin-yml';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   ...ymlPlugin.configs['flat/recommended'],
   ...ymlPlugin.configs['flat/prettier'],
+  {
+    extends: [markdown.configs.recommended, markdown.configs.processor],
+    files: ['**/*.md'],
+    language: 'markdown/gfm',
+  },
   prettier,
 ]);
