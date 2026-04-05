600bf8ecb09bb0bd3bbb66b21072d235cd12e379
Fernando Pasik <fernando@pasik.com.ar>
2026-04-05 16:03:30 +0000

    style: update config

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default defineConfig([
     language: 'markdown/gfm',
   },
   {
-    extends: [ymlConfigs['flat/standard'], ymlConfigs['flat/prettier']],
+    extends: [ymlConfigs.standard, ymlConfigs.prettier],
     files: ['*.yaml', '*.yml'],
   },
   prettier,
